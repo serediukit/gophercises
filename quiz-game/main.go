c693d36757fcb2dc98222317ef40f0e40b3dc7b2
serediukit <serediukit@gmail.com>
2025-04-02 23:36:13 +0300

    add quiz logic

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-const (
-	filePath = "problems.csv"
+var (
+	filePath = flag.String("filePath", "problems.csv", "The path to file where questions are containing")
+	limit    = flag.Int("limit", 30, "The time in second that you will have to complete the quiz")
 )
 
 func main() {
-	file, err := os.Open(filePath)
+	flag.Parse()
+
+	file, err := os.Open(*filePath)
 	if err != nil {
 		fmt.Println("Cannot read file:", err)
+		return
 	}
 
 	defer func(file *os.File) {
@@ -29,7 +36,27 @@ func main() {
 	records, err := reader.ReadAll()
 	if err != nil {
 		fmt.Println("Cannot read csv file:", err)
+		return
 	}
 
-	fmt.Println(records)
+	scanner := bufio.NewScanner(os.Stdin)
+	countCorrect, countTotal := 0, len(records)
+
+	for i, record := range records {
+		fmt.Printf("Problem #%d: %s = ", i+1, record[0])
+
+		scanner.Scan()
+		input := scanner.Text()
+		input = strings.TrimSpace(input)
+		input = strings.ToLower(input)
+
+		answer := strings.TrimSpace(record[1])
+		answer = strings.ToLower(answer)
+
+		if answer == input {
+			countCorrect++
+		}
+	}
+
+	fmt.Printf("Correct Answers is %d out of %d\n", countCorrect, countTotal)
 }
